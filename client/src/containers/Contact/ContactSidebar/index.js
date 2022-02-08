1300285976962b3c9e543116898188fd945f0472
doduy291 <doduy291@gmail.com>
2022-02-08 16:29:24 +0700

    Client - Search contact feature

--- client/src/containers/Contact/ContactSidebar/index.js
+++ client/src/containers/Contact/ContactSidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { PeopleAlt, Search } from '@mui/icons-material';
 import { Avatar } from '@mui/material';
 import {
@@ -12,24 +12,31 @@ import {
 } from './styles';
 import { PersonAdd } from '@mui/icons-material';
 import DialogAddContact from '../../../components/Dialog/AddContact';
-import { filteredContactChannel } from '../data';
 import { debounce } from '../../../utils/helper';
+import { useEffect } from 'react';
 
-const Sidebar = React.memo(({ allContacts, user, setAllContacts }) => {
+const Sidebar = React.memo(({ allContacts }) => {
   const [openDialogAddContact, setOpenDialogAddContact] = useState(false);
-  const searchContactRef = useRef();
+  const [contacts, setContacts] = useState();
+  const inputRef = useRef();
+  const typingTimeoutRef = useRef();
+
   const openDialogHandler = () => {
     setOpenDialogAddContact(true);
   };
-  const searchKeyUp = () => {
-    const keyword = searchContactRef.current.value;
+
+  const searchContactHandler = () => {
     const searchContacts = () => {
-      const filteredContact = allContacts?.contacts?.filter((contact) => contact.username.match(keyword));
-      console.log(filteredContact);
-      setAllContacts(filteredContact);
+      const keyword = inputRef.current.value;
+      const filteredContact = allContacts?.filter((contact) => contact.username.match(keyword));
+      setContacts(filteredContact);
     };
-    debounce(500, searchContacts());
+    debounce(searchContacts, 500, typingTimeoutRef);
   };
+
+  useEffect(() => {
+    setContacts(allContacts);
+  }, [allContacts]);
 
   return (
     <>
@@ -37,10 +44,10 @@ const Sidebar = React.memo(({ allContacts, user, setAllContacts }) => {
         <SidebarTitle className="contact-sidebar__title">
           Contacts
           <PeopleAlt />
-          <div className="count">{allContacts?.contacts?.length}</div>
+          <div className="count">{contacts?.length}</div>
         </SidebarTitle>
         <SidebarSearch className="contact-sidebar__search">
-          <input type="text" placeholder="Search contact" ref={searchContactRef} onKeyUp={searchKeyUp} />
+          <input type="text" placeholder="Search contact" ref={inputRef} onChange={searchContactHandler} />
           <Search />
         </SidebarSearch>
         <SidebarAddContact className="contact-sidebar__add-contact">
@@ -50,12 +57,8 @@ const Sidebar = React.memo(({ allContacts, user, setAllContacts }) => {
           </AddButton>
         </SidebarAddContact>
         <SidebarList className="contact-sidebar__list">
-          {allContacts?.contacts?.map((element, i) => (
-            <SidebarItem
-              className="contact-sidebar__item"
-              key={i}
-              to={filteredContactChannel(element._id, user._id, allContacts?.chatChannels)}
-            >
+          {contacts?.map((element, i) => (
+            <SidebarItem className="contact-sidebar__item" key={i} to={`/channel/${element.channelId}`}>
               <Avatar />
               <span>{element.username}</span>
             </SidebarItem>
